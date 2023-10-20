26c02004f16a3e2f6891afdd86606f6a6f70e702
John Watson <john@familywatson.co.uk>
2023-10-20 16:00:23 +0100

    chore: Add example crate for monitoring cli

---
+++ week-2/jtop/target/debug/deps/jtop-403caf249a40d438.d
@@ -0,0 +1,5 @@
+/Users/johnwatson/working-folder/2-repos/rust-course-tasks/week-2/jtop/target/debug/deps/jtop-403caf249a40d438: src/main.rs
+
+/Users/johnwatson/working-folder/2-repos/rust-course-tasks/week-2/jtop/target/debug/deps/jtop-403caf249a40d438.d: src/main.rs
+
+src/main.rs:
---
+++ week-2/jtop/target/debug/jtop.d
@@ -0,0 +1,1 @@
+/Users/johnwatson/working-folder/2-repos/rust-course-tasks/week-2/jtop/target/debug/jtop: /Users/johnwatson/working-folder/2-repos/rust-course-tasks/week-2/jtop/src/main.rs
