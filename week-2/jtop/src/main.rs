ae5c97621e606a4215ca0517566b428a99e74168
John Watson <john@familywatson.co.uk>
2023-10-20 18:58:12 +0100

    First CLI

--- week-2/jtop/src/main.rs
+++ week-2/jtop/src/main.rs
@@ -1,3 +1,16 @@
+// cargo run -- target 5
+
+fn get_arguments<'a>() -> String {
+    let args: Vec<_> = std::env::args().collect(); // get all arguements passed to app
+    println!("{:?}", args);
+    let first_arg = args.clone().into_iter().nth(0);
+    if let Some(arg) = first_arg {
+        return arg
+    }
+    return "".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let first = get_arguments();
+    println!("{}", &first);
 }
